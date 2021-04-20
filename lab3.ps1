3df242398313005e8903ae8269bc3733e5234477
James Petty <38379414+psjamesp@users.noreply.github.com>
2021-04-20 17:22:36 -0400

    Create lab3.ps1

---
+++ lab3.ps1
@@ -0,0 +1,22 @@
+function Get-jmpComputerInfo{
+    # ComputerSystem info
+    $CompInfo = Get-CimInstance Win32_ComputerSystem 
+    # OS info
+    $OSInfo = Get-CimInstance Win32_OperatingSystem 
+    # Serial No
+    $BiosInfo = Get-CimInstance Win32_BIOS 
+
+    $diskfreespace = get-volume -DriveLetter c | Select-Object -ExpandProperty SizeRemaining
+    [pscustomobject]@{
+        ComputerName = $CompInfo.Name
+        Domain = $CompInfo.Domain
+        OperatingSystem = $OSInfo.Caption
+        Model = $CompInfo.Model
+        MachineSN = $BiosInfo.SerialNumber
+        Version = $OSInfo.caption
+        BuildNumber = $OSInfo.version
+        OSArchitecture = $OSinfo.OSArchitecture
+        PowerShellVersion = $PSVersionTable.psversion
+        DiskFreeSpace = "" +[math]::Round($diskfreespace /1GB,2) + " GB"
+    }
+}
